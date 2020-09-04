6820862780c21b4a71bd435d8919756bc0095e05
nisacodelifings <nisa.codelifings@gmail.com>
2020-09-04 07:40:54 +0000

    mantap

--- MyApp.js
+++ MyApp.js
@@ -1,10 +1,23 @@
 import React from "react"
 
 function MyApp(){
+    const date = new Date()
+    const hours = date.getHours()
+    let time
+
+    if (time >= 6 && time <= 10 ){
+        time = "morning"
+    } else if (time > 10 && time <= 17 ){
+        time = "afternoon"
+    }else {
+        time = "night"
+    }
+
   return(
     <main>
-    <h1>Hello, World!</h1>
-    <p>Hey, I would Like to reccomend you something awesome to learn new framework</p>
+    <h1>Hello, <span>World!</span></h1>
+    <p>Hey, I would Like to reccomend you something awesome 
+    to learn new framework <span>in this {time}</span></p> 
     </main>
   )
 }
